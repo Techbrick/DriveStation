2e0af7eac440c80d2844fca4f199ae47f98d8487
Russell Chiu <chiurj2002@gmail.com>
2019-01-19 10:12:12 -0500

    project with frc update.

---
+++ src/main/java/frc/robot/OI.java
@@ -0,0 +1,73 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;      //MAKE Buttons
+
+import frc.robot.commands.*;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
+/**
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot.
+ */
+public class OI {
+  int pos = 0;
+  // Make Joysticks
+  Joystick joy = new Joystick(1); //port number goes here
+  Joystick driver = new Joystick(0);
+
+  //Make JoystickButtons
+  Button [] buttons = new JoystickButton [9]; 
+
+  Button up = new JoystickButton(driver, 3);
+  Button down = new JoystickButton(driver, 2);
+  Button home = new JoystickButton(driver, 0);
+  public OI(Robot r) 
+  {
+    for(int i = 0; i < buttons.length; i++)
+    {
+        buttons[i] = new JoystickButton(joy, i);
+    }
+    for(int i = 0; i < buttons.length; i++)
+    {
+      buttons[i].whenPressed(new Turn(r, i));
+    }
+      up.whenPressed(new Turn(r, 5));
+      down.whenPressed(new Turn(r, -5));
+      home.whenPressed(new Turn(r, 1));
+    
+  }
+  //// CREATING BUTTONS
+  // One type of button is a joystick button which is any button on a
+  //// joystick.
+  // You create one by telling it which joystick it's on and which button
+  // number it is.
+  // Joystick stick = new Joystick(port);
+  // Button button = new JoystickButton(stick, buttonNumber);
+   
+  // There are a few additional built in buttons you can use. Additionally,
+  // by subclassing Button you can create custom triggers and bind those to
+  // commands the same as any other Button.
+
+  //// TRIGGERING COMMANDS WITH BUTTONS
+  // Once you have a button, it's trivial to bind it to a button in one of
+  // three ways:
+
+  // Start the command when the button is pressed and let it run the command
+  // until it is finished as determined by it's isFinished method.
+  // button.whenPressed(new ExampleCommand());
+
+  // Run the command while the button is being held down and interrupt it once
+  // the button is released.
+  // button.whileHeld(new ExampleCommand());
+
+  // Start the command when the button is released and let it run the command
+  // until it is finished as determined by it's isFinished method.
+  // button.whenReleased(new ExampleCommand());
+}
